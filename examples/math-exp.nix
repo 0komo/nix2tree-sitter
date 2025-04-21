ee4072eb586e5b4e813bcda9323c15d2092a8219
Komo <afiqquraisyzulkarnain@gmail.com>
2025-04-22 07:54:28 +0800

    chore: added examples

--- examples/math-exp.nix
+++ examples/math-exp.nix
@@ -1,7 +1,84 @@
+# Based on DerekStride/tree-sitter-math
+
 with (import ../.);
+let
+  PREC = builtins.mapAttrs (name: value: prec.left value) {
+    addition = 1;
+    multiplication = 2;
+    exponent = 3;
+  };
+  FIELD = {
+    left = field "left";
+    right = field "right";
+  };
+in
 grammar {
   name = "math";
   rules = [
-    (rule "number" (s: R"\\d+(\\.\\d+)?"))
+    (rule "expression" (s: s._expression))
+    
+    (rule "_expression" (
+      s: choice [
+        s.variable
+        s.number
+        s.sum
+        s.subtraction
+        s.product
+        s.division
+        s.exponent
+        s._parenthesized_expression
+      ]
+    ))
+    
+    (rule "sum" (
+      s: PREC.addition (seq [
+        (FIELD.left s._expression)
+        "+"
+        (FIELD.right s._expression)
+      ])
+    ))
+
+    (rule "subtraction" (
+      s: PREC.addition (seq [
+        (FIELD.left s._expression)
+        "-"
+        (FIELD.right s._expression)
+      ])
+    ))
+    
+    (rule "product" (
+      s: PREC.multiplication (seq [
+        (FIELD.left s._expression)
+        "*"
+        (FIELD.right s._expression)
+      ])
+    ))
+
+    (rule "division" (
+      s: PREC.multiplication (seq [
+        (FIELD.left s._expression)
+        "/"
+        (FIELD.right s._expression)
+      ])
+    ))
+    
+    (rule "exponent" (
+      s: PREC.exponent (seq [
+        (field "base" s._expression)
+        "**"
+        (field "exponent" s._expression)
+      ])
+    ))
+    
+    (rule "_parenthesized_expression" (
+      s: seq [
+        "("
+        s._expression
+        ")"
+      ]
+    ))
+    
+    (rule "number" (s: R ''\d+(\.\d+)?''))
+    (rule "variable" (s: R ''([a-zA-Z$][0-9a-zA-Z_]*)''))
   ];
 }
